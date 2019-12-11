93e90d3d57ddc802346ae6373818de31e03e4da4
lomashbhattarai <lomashbhattarai1@gmail.com>
2019-12-11 11:39:16 +0545

    first commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Card } from '../card/card.component'
+import './card-list.styles.css';
+
+export const CardList = (props) => (
+    <div className="card-list">  
+    {props.monsters.map( monster => (
+        <Card monster={monster} />
+      ))} 
+    </div>
+)
