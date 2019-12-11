035f31fa2ab74670e70b8a1a8c767153892e950d
lomashbhattarai <lomashbhattarai1@gmail.com>
2019-12-11 11:48:04 +0545

    Updates

---
+++ precache-manifest.178887f725e35a205efda9f078aaa0a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77210ee58ce1d214c6a22347ab96bca9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "762ec7508431df34b10f",
+    "url": "/monster-rolodex/static/css/main.f2aec895.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "762ec7508431df34b10f",
+    "url": "/monster-rolodex/static/js/main.f8693073.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
